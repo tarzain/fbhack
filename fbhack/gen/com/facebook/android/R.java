bdb5a7c315783494c0d188c1483d18a8145a94e9
Andrew Harper <graphitezeppelin@gmail.com>
2012-11-03 03:28:05 -0400

    picture taking sort of works still some crashes, and messy as shit unecessary stuff flying every which way.
    Onto posting to wall.

--- fbhack/gen/com/facebook/android/R.java
+++ fbhack/gen/com/facebook/android/R.java
@@ -17,9 +17,10 @@ public final class R {
         public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070002;
-        public static final int postpictures=0x7f070000;
-        public static final int textView1=0x7f070001;
+        public static final int imageview1=0x7f070000;
+        public static final int menu_settings=0x7f070003;
+        public static final int postpictures=0x7f070001;
+        public static final int textView1=0x7f070002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- fbhack/src/com/example/fbhack/MainActivity.java
+++ fbhack/src/com/example/fbhack/MainActivity.java
@@ -1,32 +1,70 @@
 package com.example.fbhack;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import com.facebook.android.*;
 import com.facebook.android.Facebook.*;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+//NOTE THE OTHER .JAVA FILES ARE NOT REALLY USED AS OF NOW THEY ARE FROM EARLIER ATTEMPT
+//CURRENTLY SUCCESFLY TAKES A PICTURE AND PLACES IN IMG VIEW
+//TODO, MAKE THE IMAGE NOT DISPLAYED BUT RATHER POST TO FACEBOOK WALL
+// AND TOSS THE METADATA SOMEHOW
 
 public class MainActivity extends Activity {
 
-	Facebook facebook = new Facebook("157725621040134");
+	private static final int CAMERA_REQUEST = 1888; 
+    private ImageView imageView;
+    Facebook facebook = new Facebook("157725621040134");
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     
-        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);  
+        this.imageView = (ImageView)this.findViewById(R.id.imageview1);
+        Button photoButton = (Button) this.findViewById(R.id.postpictures);
+        
 
-
-    facebook.authorize(this, new DialogListener() {
+    facebook.authorize(this, new String[] { "publish_stream" }, new DialogListener() {
     	 @Override
-    	 public void onComplete(Bundle values) {}
+    	 public void onComplete(Bundle values) {
+    		 //postToWall(values.getString(Facebook.TOKEN));
+    	 }
+
+    	 
+    	 //THIS METHOD IS PROBABLY NOT WOTH SAVING
+    	 //COULDNT GET IT TO NOT CRASH
+    	 
+		/*private void postToWall(String accessToken) {
+			   Bundle params = new Bundle();
+
+			    params.putString(Facebook.TOKEN, accessToken);
+
+			    // The byte array is the data of a picture.
+			    params.putByteArray("picture", getIntent().getExtras().getByteArray("data"));
+
+			    try {
+			        facebook.request("me/photos", params, "POST");
+
+			    } catch (FileNotFoundException fileNotFoundException) {
+			    } catch (MalformedURLException malformedURLException) {
+			    } catch (IOException ioException) {
+			    }			
+		}*/
 
 		@Override
 		public void onCancel() {			
@@ -42,31 +80,44 @@ public class MainActivity extends Activity {
     
     });
     
-    OnClickListener mCorkyListener = new OnClickListener() {
+    photoButton.setOnClickListener(new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE); 
+            startActivityForResult(cameraIntent, CAMERA_REQUEST); 
+        }
+    });
+    
+
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {  
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {  
+            Bitmap photo = (Bitmap) data.getExtras().get("data"); 
+            imageView.setImageBitmap(photo);
+        }  
+    } 
+	
+  View.OnClickListener mCorkyListener = new View.OnClickListener() {
+		Intent myIntent = new Intent();
 
 		public void onClick(View view) {
-			// TODO Auto-generated method stub
-			
+			myIntent = new Intent(view.getContext(), PostActivity.class);			
 		}
 
-		@Override
-		public void onClick(DialogInterface dialog, int which) {
-			// TODO Auto-generated method stub
-			
-		}
 		};
  
     
 
     
     
-    }
+    
     
     
     public void goTo(View view){
 		Intent myIntent = new Intent();
 		switch (view.getId()){
-			case	R.id.postpictures: myIntent = new Intent(view.getContext(), PostActivity.class);
+			case	R.id.postpictures: myIntent = new Intent(view.getContext(), MakePhotoActivity.class);
 					break;
 
 		}
