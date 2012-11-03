bd34e2c7d747e2593bd066afe61dd9daf08773f4
Andrew Harper <graphitezeppelin@gmail.com>
2012-11-03 01:52:23 -0400

    commitment

--- fbhack/src/com/example/fbhack/MainActivity.java
+++ fbhack/src/com/example/fbhack/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.fbhack;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.os.Bundle;
 import com.facebook.android.*;
 import com.facebook.android.Facebook.*;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -18,6 +21,8 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);  
+
 
     facebook.authorize(this, new DialogListener() {
     	 @Override
@@ -36,7 +41,37 @@ public class MainActivity extends Activity {
 		}
     
     });
+    
+    OnClickListener mCorkyListener = new OnClickListener() {
+
+		public void onClick(View view) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onClick(DialogInterface dialog, int which) {
+			// TODO Auto-generated method stub
+			
+		}
+		};
+ 
+    
+
+    
+    
     }
+    
+    
+    public void goTo(View view){
+		Intent myIntent = new Intent();
+		switch (view.getId()){
+			case	R.id.postpictures: myIntent = new Intent(view.getContext(), PostActivity.class);
+					break;
+
+		}
+		startActivityForResult(myIntent, 0);
+	}
 
     
     
